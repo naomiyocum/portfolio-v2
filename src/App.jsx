65c03131da7192c906f2d68c027c368d3e8f7a29
Naomi Yocum <naomiyocum24@gmail.com>
2023-07-20 15:17:20 -0400

    WIP projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-import Projects from "./components/Projects";
+import Project from "./components/Project";
+import mainProjects from "./data/mainProjects.jsx";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -10,10 +11,17 @@ function App() {
     setDarkMode((prevMode) => !prevMode);
   }
 
+  const mains = mainProjects.map((proj) => {
+    return <Project key={proj.name} {...proj} darkMode={darkMode} />;
+  });
+
   return (
     <main className={darkMode ? "dark" : "light"}>
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <About darkMode={darkMode} />
+      <section className="container mx-auto p-6 grid grid-cols-2 gap-5">
+        {mains}
+      </section>
     </main>
   );
 }
