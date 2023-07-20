ae4da9fb271c1a1c6622cf5c1b058daf5f37563c
Naomi Yocum <naomiyocum24@gmail.com>
2023-07-20 15:17:10 -0400

    add projects and images

---
+++ src/data/otherProjects.jsx
@@ -0,0 +1,21 @@
+const otherProjects = [
+  {
+    name: "Portfolio V1",
+    image: "",
+    github: "https://github.com/naomiyocum/portfolio",
+    live: "https://naomiyocum.github.io/portfolio/",
+    summary: "The first version of my portfio deployed using GitHub Pages",
+    technologies: ["HTML", "CSS", "Vanilla JavaScript"],
+  },
+  {
+    name: "Battleship",
+    image: "",
+    github: "https://github.com/naomiyocum/battleship",
+    live: "",
+    summary:
+      "A command-line interface (CLI) Battleship game - you vs the computer",
+    technologies: ["Ruby", "RSpec", "Pry"],
+  },
+];
+
+export default otherProjects;
