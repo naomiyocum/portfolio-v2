1abcfb1f8b6b90620140c1f840c0f756846bfb2e
Naomi Yocum <naomiyocum24@gmail.com>
2023-07-20 10:33:36 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Naomi's Portfolio V2
+      </h1>
+    </div>
+  );
+}
+
+export default App;
